b04d8d1cdb3712fb120b513c9583f3d6bdd59d1c
msivakumarreddy <info@joindevops.com>
2025-07-10 01:51:35 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "my_ec2" {
+  # Leave it mostly empty for now
+  ami = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = ["sg-0b04bed9fb3c61913"]
+  instance_type = "t3.micro"
+  tags =  {
+    Name = "linux-1"
+  }
+}
